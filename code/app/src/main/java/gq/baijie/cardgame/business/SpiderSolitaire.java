2094db54bb1cf9e9d56c985a32a9309febd60f48
Bai Jie <baijie1991@gmail.com>
2016-07-15 21:20:25 +0800

    initialize SpiderSolitaire.cardStacks by constructor

--- code/app/src/main/java/gq/baijie/cardgame/business/SpiderSolitaire.java
+++ code/app/src/main/java/gq/baijie/cardgame/business/SpiderSolitaire.java
@@ -1,6 +1,7 @@
 package gq.baijie.cardgame.business;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import gq.baijie.cardgame.domain.entity.Card;
@@ -9,13 +10,22 @@ public class SpiderSolitaire {
 
   public static class State {
 
-    final List<CardStack> cardStacks = new ArrayList<>(10);
+    /** left to right, 0 to 9 */
+    public final List<CardStack> cardStacks;
 
     /** cards for drawing, initially in five piles of ten with no cards showing */
-    final List<Card> cardsForDrawing = new ArrayList<>(50);
+    public final List<Card> cardsForDrawing = new ArrayList<>(50);
+
+    public State() {
+      List<CardStack> cardStacks = new ArrayList<>(10);
+      for (int i = 0; i < 10; i++) {
+        cardStacks.add(new CardStack());
+      }
+      this.cardStacks = Collections.unmodifiableList(cardStacks);
+    }
 
     public static class CardStack {
-      List<Card> cards = new ArrayList<>();
+      public List<Card> cards = new ArrayList<>();
       /** hide: [0, openIndex), open: [openIndex, cards.size()) */
       int openIndex;
     }
