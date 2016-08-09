c6bbf4ef91a0dee5298311504cb3c036a19eb58c
Bai Jie <baijie1991@gmail.com>
2016-08-09 23:31:34 +0800

    improve the UI of AndroidSortedCardsView

--- code/client/android/src/main/java/gq/baijie/cardgame/client/android/ui/view/AndroidSortedCardsView.java
+++ code/client/android/src/main/java/gq/baijie/cardgame/client/android/ui/view/AndroidSortedCardsView.java
@@ -1,17 +1,15 @@
 package gq.baijie.cardgame.client.android.ui.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.view.Gravity;
-import android.widget.TextView;
 
 import gq.baijie.cardgame.client.android.R;
+import gq.baijie.cardgame.client.android.ui.widget.CardStackLayout;
+import gq.baijie.cardgame.domain.entity.Card;
 import gq.baijie.cardgame.facade.view.SortedCardsView;
 import rx.Observable;
 
-public class AndroidSortedCardsView extends TextView implements SortedCardsView {
+public class AndroidSortedCardsView extends CardStackLayout implements SortedCardsView {
 
   public AndroidSortedCardsView(Context context) {
     super(context);
@@ -28,22 +26,23 @@ public class AndroidSortedCardsView extends TextView implements SortedCardsView 
     init();
   }
 
-  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-  public AndroidSortedCardsView(Context context, AttributeSet attrs, int defStyleAttr,
-                                int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-    init();
-  }
-
   private void init() {
-    setGravity(Gravity.CENTER);
-    setBackgroundResource(R.drawable.card_background);
+    setDelta(getResources().getDimensionPixelSize(R.dimen.piled_cards_delta));
   }
 
   @Override
   public void setDecks(int decks) {
-    setText(String.valueOf(decks));
     setVisibility(decks > 0 ? VISIBLE : INVISIBLE);
+    // * let getChildCount() == decks
+    if (getChildCount() < decks) {
+      // add cards
+      while (getChildCount() < decks) {
+        addView(new AndroidCardView(getContext(), new Card(Card.Suit.HEART, Card.Rank.ACE), true));
+      }
+    } else if (getChildCount() > decks) {
+      // remove cards
+      removeViews(decks, getChildCount() - decks);
+    } // else getChildCount() == decks, do nothing
   }
 
   @Override
