43a7a615563ec6485f8fbdc5660b695c118ff573
Bai Jie <baijie1991@gmail.com>
2016-08-09 23:31:34 +0800

    improve the UI of AndroidDrawingCardsView

--- code/client/android/src/main/java/gq/baijie/cardgame/client/android/ui/view/AndroidDrawingCardsView.java
+++ code/client/android/src/main/java/gq/baijie/cardgame/client/android/ui/view/AndroidDrawingCardsView.java
@@ -1,18 +1,16 @@
 package gq.baijie.cardgame.client.android.ui.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.view.Gravity;
-import android.widget.TextView;
 
 import gq.baijie.cardgame.client.android.R;
+import gq.baijie.cardgame.client.android.ui.widget.CardStackLayout;
+import gq.baijie.cardgame.domain.entity.Card;
 import gq.baijie.cardgame.facade.view.DrawingCardsView;
 import gq.baijie.cardgame.facade.view.ViewHelper;
 import rx.Observable;
 
-public class AndroidDrawingCardsView extends TextView implements DrawingCardsView {
+public class AndroidDrawingCardsView extends CardStackLayout implements DrawingCardsView {
 
   private final ViewHelper viewHelper = new ViewHelper();
 
@@ -31,23 +29,25 @@ public class AndroidDrawingCardsView extends TextView implements DrawingCardsVie
     init();
   }
 
-  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-  public AndroidDrawingCardsView(Context context, AttributeSet attrs, int defStyleAttr,
-                                 int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-    init();
-  }
-
   private void init() {
-    setGravity(Gravity.CENTER);
-    setBackgroundResource(R.drawable.card_background);
+    setDelta(getResources().getDimensionPixelSize(R.dimen.piled_cards_delta));
     super.setOnClickListener(v -> viewHelper.nextEvent(new DrawEvent(this)));
   }
 
   @Override
   public void setDecks(int decks) {
-    setText(String.valueOf(decks));
     setVisibility(decks > 0 ? VISIBLE : INVISIBLE);
+    // * let getChildCount() == decks
+    if (getChildCount() < decks) {
+      // add cards
+      while (getChildCount() < decks) {
+        addView(new AndroidCardView(
+            getContext(), new Card(Card.Suit.HEART, Card.Rank.KING), false));
+      }
+    } else if (getChildCount() > decks) {
+      // remove cards
+      removeViews(decks, getChildCount() - decks);
+    } // else getChildCount() == decks, do nothing
   }
 
   @Override
