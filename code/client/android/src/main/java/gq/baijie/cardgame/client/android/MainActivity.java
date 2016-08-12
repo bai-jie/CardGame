794598ccb28d135033b318e8a308df219d496376
Bai Jie <baijie1991@gmail.com>
2016-08-12 20:32:01 +0800

    let user exit through menu instead of BackPressed
    resolves #4

--- code/client/android/src/main/java/gq/baijie/cardgame/client/android/MainActivity.java
+++ code/client/android/src/main/java/gq/baijie/cardgame/client/android/MainActivity.java
@@ -2,6 +2,7 @@ package gq.baijie.cardgame.client.android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import gq.baijie.cardgame.business.SpiderSolitaires;
 import gq.baijie.cardgame.client.android.ui.view.AndroidDrawingCardsView;
@@ -28,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
         new AndroidSortedCardsView(this),
         new AndroidGameCompleteView(this)
     );
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    menu.add(R.string.action_exit).setOnMenuItemClickListener(item -> {
+      finish();
+      return true;
+    });
+    return true;
+  }
+
+  @Override
+  public void onBackPressed() {
+    // don't finish()
+    // let user finish() through "Exit" menu item
   }
 
 }
